1287a363816888e5fe26a778dd61b6db662cb591
simone-danzeri <s.danzeri@icloud.com>
2024-04-30 13:22:09 +0200

    feat: creato primo file con html base e form

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+  <meta charset='UTF-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <title>Document</title>
+</head>
+<body>
+  <form action="badwords.php" method="get">
+    <div>
+        <label for="paragraph">Il tuo paragrafo:</label>
+        <input name="paragraph" type="text" placeholder="Scrivi qui il tuo paragrafo">
+    </div>
+    <div>
+        <label for="badword">La parola da censurare è:</label>
+        <input name="badword" type="text" placeholder="Scrivi qui il tua parola">
+    </div>
+  </form>
+</body>
+</html>
