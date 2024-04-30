bfd3464d19a50a0f575945ea42e0140db89428d7
simone-danzeri <s.danzeri@icloud.com>
2024-04-30 13:41:22 +0200

    fix: mancavano qualche ';'

--- badwords.php
+++ badwords.php
@@ -2,7 +2,7 @@
 <?php 
 $paragraph = $_GET['paragraph'];
 $badWord = $_GET['badword'];
-$censuredParagraph = str_replace($badWord, '***', $paragraph)
+$censuredParagraph = str_replace($badWord, '***', $paragraph);
 ?>
 
 <!-- HTML -->
@@ -16,15 +16,15 @@ $censuredParagraph = str_replace($badWord, '***', $paragraph)
 <body>
 <div>
     <p>
-       <strong>Il paragrafo che hai scritto è: </strong> <?php echo $paragraph ?>
+       <strong>Il paragrafo che hai scritto è: </strong> <?php echo $paragraph; ?>
     </p>
-    <span><strong>E la sua lungheza è di </strong> <?php echo strlen($paragraph) ?> <strong> caratteri.</strong></span>
+    <span><strong>E la sua lungheza è di </strong> <?php echo strlen($paragraph); ?> <strong> caratteri.</strong></span>
 </div>
 <div>
     <p>
-        <strong>Ecco il testo censurato: </strong> <?php echo $censuredParagraph ?>
+        <strong>Ecco il testo censurato: </strong> <?php echo $censuredParagraph; ?>
     </p>
-    <span><strong>E la sua lungheza è di </strong> <?php echo strlen($censuredParagraph) ?> <strong> caratteri.</strong></span>
+    <span><strong>E la sua lungheza è di </strong> <?php echo strlen($censuredParagraph); ?> <strong> caratteri.</strong></span>
 </div>
 </body>
 </html>
